73ebb0985cf46005dc9c88351cb8f741d5cb3abd
Gregory Clark <clarkgregh@gmail.com>
2018-07-02 22:36:55 -0600

    Fixed bug where notification settings checkboxes were set to false(unchecked) by default.

--- src/components/Profile/Prof_Notifications/Prof_Notifications.js
+++ src/components/Profile/Prof_Notifications/Prof_Notifications.js
@@ -5,37 +5,68 @@ import { connect } from 'react-redux';
 import axios from 'axios';
 
 class Prof_Notifications extends Component {
+    constructor(){
+        super()
+        this.state = {
+            emailEnabled:true,
+            textEnabled:true,
+            inAppEnabled:true
+        }
+
+        
+    }
+    componentWillMount(){
+        this.setState({
+            emailEnabled:this.props.user.email_notify,
+            textEnabled: this.props.user.text_notify,
+            inAppEnabled: this.props.user.in_app_notify
+            
+        })
+    }
+    toggleEmail(e) {
+        this.setState({
+            emailEnabled: !this.state.emailEnabled
+        })
+        let toggleObj = {
+            user_id: this.props.userID,
+            notify_email: e.target.checked,
+        }
+        axios.put('/email_notifications_settings', toggleObj).then(() => {
+            //should I have something here?
+        })
+    }
+    toggleText(e) {
+        this.setState({
+            textEnabled: !this.state.textEnabled
+        })
+        let toggleObj = {
+            user_id: this.props.userID,
+            notify_text: e.target.checked,
+        }
+        axios.put('/text_notifications_settings', toggleObj).then(() => {
+            //should I have something here?
+        })
+    }
+    toggleInApp(e) {
+        this.setState({
+            inAppEnabled: !this.state.inAppEnabled
+        })
+        let toggleObj = {
+            user_id: this.props.userID,
+            notify_inApp: e.target.checked,
+        }
+        axios.put('/in_app_notifications_settings', toggleObj).then(() => {
+            //should I have something here?
+        })
+    }
 
     render() {
-        const userID = this.props.userID;
-        function toggleEmail(e) {
-            // e.target.checked === true ? console.log(e.target.checked, "it's true") : console.log(e.target.checked, "it's false");
-            let toggleObj = {
-                user_id: userID,
-                notify_email: e.target.checked,
-            }
-            axios.put('/email_notifications_settings', toggleObj).then(() => {
-                //should I have something here?
-            })
-        }
-        function toggleText(e) {
-            let toggleObj = {
-                user_id: userID,
-                notify_text: e.target.checked,
-            }
-            axios.put('/text_notifications_settings', toggleObj).then(() => {
-                //should I have something here?
-            })
-        }
-        function toggleInApp(e) {
-            let toggleObj = {
-                user_id: userID,
-                notify_inApp: e.target.checked,
-            }
-            axios.put('/in_app_notifications_settings', toggleObj).then(() => {
-                //should I have something here?
-            })
-        }
+        const userID = this.props;
+        console.log(userID)
+        
+
+
+
 
 
         return (
@@ -45,19 +76,19 @@ class Prof_Notifications extends Component {
 
                 <h3>Email</h3>
                 <label className="switch">
-                    <input type="checkbox" id="email_check"  onChange={(e) => toggleEmail(e)} />
+                    <input type="checkbox" id="email_check" checked={this.state.emailEnabled} onChange={(e) => this.toggleEmail(e)} />
                     <span className="slider round"></span>
                 </label>
 
                 <h3>SMS Text</h3>
                 <label className="switch">
-                    <input type="checkbox" id="text_check" onChange={(e) => toggleText(e)} />
+                    <input type="checkbox" id="text_check" checked={this.state.textEnabled} onChange={(e) => this.toggleText(e)} />
                     <span className="slider round"></span>
                 </label>
 
                 <h3>In-App</h3>
                 <label className="switch">
-                    <input type="checkbox" id="inApp_check" onChange={(e) => toggleInApp(e)} />
+                    <input type="checkbox" id="inApp_check" checked={this.state.inAppEnabled} onChange={(e) => this.toggleInApp(e)} />
                     <span className="slider round"></span>
                 </label>
 

482912f8fd2e620081c7fe57193482d33e240b49
Gregory Clark <clarkgregh@gmail.com>
2018-11-10 19:11:28 -0700

    removed some old code that had been commented out

--- src/components/Reminders/Templates/Templates.js
+++ src/components/Reminders/Templates/Templates.js
@@ -4,8 +4,6 @@ import { getUser, getFrequencies, getUserReminders, getUserReminderTemplates } f
 import { connect } from 'react-redux';
 import TemplatesTable from './TemplatesTable/TemplatesTable';
 
-// import Button from '@material-ui/core/Button';
-
 class Templates extends Component {
 
     componentDidMount() {
@@ -31,38 +29,10 @@ class Templates extends Component {
 
     render() {
 
-        // let templatesList = this.props.userReminderTemplates.length? this.props.userReminderTemplates.map((template, index) => {
-
-        //     if (template.first_instance_date && template.is_deleted === false) {
-        //         return <tr key={index}>
-        //             <td>{template.title}</td>
-        //             <td>{template.first_instance_date.substring(0, 10)}</td>
-        //             <td>{template.frequency}</td>
-        //             <td><button value={template.id} onClick={(e) => { this.editTemplate(e.target.value) }}>Edit</button>
-        //                 <button value={template.id} onClick={(e) => { this.deleteTemplate(e.target.value) }}>Delete</button>
-        //             </td>
-        //         </tr>
-        //     }
-        //     else { return null }
-
-
-        // })
-        // :null
         return (
             <div className="Templates_master">
             <TemplatesTable reminderTemplates = {this.props.userReminderTemplates}/>
 
-                {/* <table className="reminders_table">
-                    <tbody>
-                        <tr>
-                            <th>Title</th>
-                            <th>Initial Date</th>
-                            <th>Frequency</th>
-                            <th>Other</th>
-                        </tr>
-                        {templatesList}
-                    </tbody>
-                </table> */}
             </div>
         )
     }
